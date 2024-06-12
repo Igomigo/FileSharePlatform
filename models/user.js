0fdbddf1d2867a171c157e530e5ec09b5e1e82a3
Igomigo <igomigofatai@gmail.com>
2024-06-12 17:40:59 +0100

    Code: User model created

---
+++ models/user.js
@@ -0,0 +1,27 @@
+// User model setup
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        maxLength: 60,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const userModel = mongoose.model(userSchema);
+
+
+module.exports = userModel;
