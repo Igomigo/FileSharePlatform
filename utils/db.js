c1e99129464a0d8172a72139a21ffe2a9adefa44
Igomigo <igomigofatai@gmail.com>
2024-06-12 15:32:08 +0100

    Code: Mongo client/connection setup

--- utils/db.js
+++ utils/db.js
@@ -1,51 +1,11 @@
 // Contains the mongodb database connection
+const mongoClient = require("../config/mongoClient");
 
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-//const dbConfig = async () => {
-//    try {
-//        console.log("Establishing a connection with the mongodb server");
-//        await mongoose.connect(process.env.MONGODB_URI);
-//        console.log("Client connected to the mongodb server successfully");
-//    } catch (err) {
-//        console.log(`Error while establishing connection: ${err}`);
-//    }
-//}
-
-class DBClient {
-    constructor() {
-        // Initializes the database class
-        this.isConnected = false;
-        this.connect = async () => {
-            try {
-                console.log("Establishing a connection to the mongodb server");
-                await mongoose.connect(process.env.MONGODB_URI);
-                console.log("Client connected to the mongodb server");
-                this.isConnected = true;
-            } catch (err) {
-                console.log(
-                    `Error while connecting to the mongodb server: ${err}`);
-            }
-        }
-    }
-
-    isAlive() {
-        // Returns true if mongodb connection is established
-        return this.isConnected;
-    }
-
-    async nbUsers() {
-        // returns the number of documents in the collection `user`
-    }
-
-    async nbFiles() {
-        // returns the number of documents in the collection `file`
-    }
+exports.nbUsers = async () => {
+    // Returns the number of documents in the collection `user`
+    return this.isConnected;
 }
 
-const mongoClient = new DBClient();
-
-module.exports = mongoClient;
+exports.nbFiles = async () => {
+        // returns the number of documents in the collection `file`
+}
